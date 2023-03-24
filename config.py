1e1270ce1613f4b781dbe441a7c91d6fb9113739
amanqs <116822442+amanqs@users.noreply.github.com>
2023-03-24 07:29:24 +0700

    n

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ pic_girl = os.getenv("PIC_GIRL", "https://telegra.ph/file/cb885bcbf5081dbd45f27.
 # =========================================================== #
 
 pesan_join = os.getenv("PESAN_JOIN", "Tidak Dapat Diakses Harap Join Terlebih Dahulu")
-start_msg = os.getenv("START_MSG", "Hai {mention} 🌱\n\n<b>SM Menfess Bot</b> adalah Bot Auto Post, Semua Pesan Yang Kamu Kirim Akan Masuk Ke Channel @smmenfess Secara Anonymous. Untuk Bantuan Ketik /help")
+start_msg = os.getenv("START_MSG", "Hai {mention} 🌱\n\n<b>FWB AWSMxCIB</b> adalah Bot Auto Post, Semua Pesan Yang Kamu Kirim Akan Masuk Ke Channel @usernamech Secara Anonymous. Untuk Bantuan Ketik /help")
 
 gagalkirim_msg = os.getenv("GAGAL_KIRIM", """
 {mention}, Pesan Mu Gagal Terkirim Silahkan Gunakan Hashtag Berikut:
